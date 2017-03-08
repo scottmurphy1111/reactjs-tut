27ad8b5c2f23ffd92b0847e55a200df5399f6f3c
Scott Murphy <scott.murphy@martinagency.com>
2017-03-08 15:17:23 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div id="errors"></div>
+        <div id="container"></div>
+      </div>
+    );
+  }
+}
+
+export default App;
